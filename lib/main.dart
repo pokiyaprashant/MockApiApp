e373955679c0b283a92fb40352aff2932a2d2324
pokiyaprashant <prashantpokiya33333@gmail.com>
2022-01-06 15:46:41 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+////////
+
+import 'package:flutter/material.dart';
+import 'package:mockapi_app/Pages/task_list.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TaskList(),
+    );
+  }
+}
